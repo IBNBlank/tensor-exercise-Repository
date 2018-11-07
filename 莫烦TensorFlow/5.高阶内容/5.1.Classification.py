eebd1a333673b5c46200ced78298df19543ef029
IBNBlank <847235539@qq.com>
2018-11-08 01:49:19 +0800

    2018/11/08
    Signed-off-by: IBNBlank <847235539@qq.com>

--- 莫烦TensorFlow/5.高阶内容/5.1.Classification.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: IBNBlank
-# @Date:   2018-11-06 23:59:53
-# @Last Modified by:   IBNBlank
-# @Last Modified time: 2018-11-07 00:00:15
-
-import tensorflow as tf
--- 莫烦TensorFlow/templates/ANN/ANN_with_tensorboard/Open_Tensorboard.bat
+++
@@ -1,3 +0,0 @@
-rd /s /Q log
-python template_with_tensorboard.py
-tensorboard --logdir="log/" --port=6006 --debugger_port=6064
